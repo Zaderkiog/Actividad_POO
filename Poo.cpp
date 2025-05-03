be42a697fca5f7b714ebce021027de12fa51c2e2
Cristian <sansmexe@gmail.com>
2025-05-02 20:08:23 -0500

    Update

--- Poo.cpp
+++ Poo.cpp
@@ -2,11 +2,17 @@
 #include <string>
 using namespace std;
 
+class Fecha {
+
+};
+
 class Empleado {
     protected:
     string nombre;
     int base;
-    private:
+    long identificacion;
+    public:
+    Empleado(string _nombre, int _base, long _identificacion): nombre(_nombre), base(_base), identificacion(_identificacion){}
 
     virtual void CalcularSalario() {
         
@@ -15,6 +21,20 @@ class Empleado {
     virtual void MostrarInfo(){
         cout << "..." << endl;
     }
+};
+
+class Temporal: public Empleado {
+  private:
+   Fecha fechaFinalizacion;
+   int salud;
+   int pension;
+   int liquidacion;
+  public:
+  Temporal(string _nombre, int _base, int _identificacion, Fecha _fechaFinalizacion, int _salud, int _pension, int _liquidacion): Empleado(_nombre, _base, _identificacion){}
+
+  void CalcularSalario() override {
+     cout << base + salud + pension + liquidacion << endl;
+  }
 };
 
 int main(){
