429c1685e78bb1ad70b5146568a774f22c761d26
Cristian <sansmexe@gmail.com>
2025-05-02 19:52:41 -0500

    Update

--- Poo.cpp
+++ Poo.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
+class Empleado {
+    protected:
+    string nombre;
+    int base;
+    private:
+
+    virtual void CalcularSalario() {
+        
+    }
+
+    virtual void MostrarInfo(){
+        cout << "..." << endl;
+    }
+};
 
 int main(){
     return 0;
