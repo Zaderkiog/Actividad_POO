82d45b550b215fcd98d1f799a4086c945f623a3a
Cristian <sansmexe@gmail.com>
2025-05-03 19:42:47 -0500

    Update

--- Poo.cpp
+++ Poo.cpp
@@ -3,40 +3,6 @@
 #include <vector>
 using namespace std;
 
-class Organizacion {
- private:
-   string name;
-   Nomina nomina;
-   int nit;
-};
-
-class Nomina {
- private:
- vector<Empleado*> Empleados;
- Organizacion* organizacion;
- public:
- /* & : El parametro de _empleados es una referencia constante a un vector de punteros a Empleado
-  Evita copiar el vector y garantiza que no se modificara dentro del constructor.
-  &_empleados referencia a un espacio de memoria de X empleado (Por ejemplo Empleado("Juan")), el apuntador toma ese espacio de memoria referenciado
-  para evitar trabajar con una copia de empleado y asi trabajar con el espacio de memoria. Los empleados solo se
-  podran leer mas no modificar. 
-  
-  Referente al puntero de organizacion, este SI crea una copia de esa organizacon y ya no trabaja con el espacio
-  de memoria*/
- Nomina(const vector<Empleado*> &_empleados, Organizacion* _origanizacion): Empleados(_empleados), organizacion(_origanizacion) {}
-
- void CalcularValor(){}
- void listarTodos(){}
- void EmpleadodelMes(){}
- void EmpleadoMasCostoso(){}
-};
-
-class Fecha {
-   private:
-   int dia;
-   int mes;
-   int anio;
-};
 
 class Empleado {
     protected:
@@ -54,6 +20,52 @@ class Empleado {
         cout << "..." << endl;
     }
 };
+
+class Nomina;
+
+class Organizacion {
+    private:
+      string name;
+      Nomina* nomina;
+      int nit;
+    public:
+    Organizacion(string _name, Nomina* _nomina, int _nit){}
+
+    void setNomina(Nomina* _nomina) {
+        nomina = _nomina;
+    }
+   };
+
+class Nomina {
+    private:
+    vector<Empleado*> Empleados;
+    Organizacion* organizacion;
+    public:
+    /* & : El parametro de _empleados es una referencia constante a un vector de punteros a Empleado
+     Evita copiar el vector y garantiza que no se modificara dentro del constructor.
+     &_empleados referencia a un espacio de memoria de X empleado (Por ejemplo Empleado("Juan")), el apuntador toma ese espacio de memoria referenciado
+     para evitar trabajar con una copia de empleado y asi trabajar con el espacio de memoria. Los empleados solo se
+     podran leer mas no modificar. 
+     
+     Referente al puntero de organizacion, este SI crea una copia de esa organizacon y ya no trabaja con el espacio
+     de memoria*/
+    Nomina(const vector<Empleado*> &_empleados, Organizacion* _origanizacion): Empleados(_empleados), organizacion(_origanizacion) {}
+   
+    void CalcularValor(){}
+    void listarTodos(){}
+    void EmpleadodelMes(){}
+    void EmpleadoMasCostoso(){}
+   };
+   
+
+class Fecha {
+   private:
+   int dia;
+   int mes;
+   int anio;
+};
+
+
 
 class Temporal: public Empleado {
   private:
@@ -74,6 +86,10 @@ class Contratista: public Empleado {
     int salud;
   public:
    Contratista(string _nombre, int _base, int _identificacion, int _salud): Empleado(_nombre, _base, _identificacion){}
+
+   void CalcularSalario() override {
+     cout << base - salud << endl;
+   }
 };
 
 class Planta: public Empleado {
@@ -85,10 +101,21 @@ class Planta: public Empleado {
    int transporte;
   public:
   Planta(string _nombre, int _base, int _identificacion, int _vaciones, int _cesantias, int _salud, int _pension, int _transporte): Empleado(_nombre, _base, _identificacion){}
+
+  void CalcularSalario() override {
+     cout << "Total: " << cesantias + (vacaciones * (base/30)) + salud + pension + transporte << endl;
+  }
 };
 
 class Practicante: public Empleado {
-   
+   private:
+      int salud;
+   public:
+   Practicante(string _name, int _base, int _identificacion, int _salud): Empleado(_name, _base, _identificacion){}
+
+   void CalcularSalario() override {
+     cout << "Total: " <<   base  - (base *0.1)  + salud << endl;
+   }
 };
 
 int main(){
